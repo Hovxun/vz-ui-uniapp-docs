80b1a373c0aa3f61cd0fdff3f55f790c6467b3b3
Hovxun <Hovxun@163.com>
2026-01-05 00:03:55 +0800

    Updates

---
+++ demo/assets/query.XN79g1Ph.js
@@ -0,0 +1,1 @@
+import{V as e}from"./index-KaPiaQMI.js";function n(n){return new Promise(((r,t)=>{const{selector:s,scope:l,all:o}=n;(l?e().in(l):e())[o?"selectAll":"select"](s).boundingClientRect().exec((([e])=>{o&&Array.isArray(e)&&e.length||!o&&e?r(e):t()}))}))}export{n as g};

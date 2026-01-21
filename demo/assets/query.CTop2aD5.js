136276ec1b50af9aee50f7b8a53dde4e61db9eff
Hovxun <Hovxun@163.com>
2026-01-21 23:57:59 +0800

    Updates

---
+++ demo/assets/query.CTop2aD5.js
@@ -0,0 +1,1 @@
+import{X as e}from"./index-0CGj8KL2.js";function n(n){return new Promise(((r,t)=>{const{selector:s,scope:l,all:o}=n;(l?e().in(l):e())[o?"selectAll":"select"](s).boundingClientRect().exec((([e])=>{o&&Array.isArray(e)&&e.length||!o&&e?r(e):t()}))}))}export{n as g};
--- demo/assets/uni-app.es.sUn1MfvE.js
+++
@@ -1,1 +0,0 @@
-import"./index-COa3cl2P.js";function r(r,t){return"string"==typeof r?t:r}export{r};
